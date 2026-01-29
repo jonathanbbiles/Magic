bd755dd2d638839f1fdb01038b1f3f8c2aa77630
jonathanbbiles <jonathanbbiles@gmail.com>
2026-01-28 18:03:40 -0800

    Remove expo-status-bar usage

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import DashboardScreen from './src/screens/DashboardScreen';
 import theme from './src/styles/theme';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar style="light" />
+      <StatusBar barStyle="light-content" />
       <DashboardScreen />
     </SafeAreaView>
   );
